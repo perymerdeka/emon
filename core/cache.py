fc7374dcc0c1eff2fd04d2342d51e7ef5233ce36
perymerdeka <ferilukmansyah@gmail.com>
2025-05-12 19:53:31 +0700

    Adding Redis Support

--- core/cache.py
+++ core/cache.py
@@ -2,9 +2,11 @@ from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 from core.config import settings
+import os
 
 async def setup_cache():
-    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    redis_url = os.getenv("REDIS_URL", "redis://localhost")
+    redis = aioredis.from_url(redis_url, encoding="utf8", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
 
 def cache_key_builder(
